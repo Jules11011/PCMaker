a5460d2199bd09e6d5d92092424fd07ea58270d2
Jules Hoeberechts <476661@student.fontys.nl>
2025-04-16 16:26:05 +0200

    Added the Logic layer (class library) with the account class. Added a razorepage for the account sign in.

---
+++ WebApp/WebApplication-PCMaker/PCMaker - Logic layer/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCMaker___Logic_layer
+{
+    public class Account
+    {
+        private int accountID;
+        private string name;
+        private string email;
+        private string password;
+        private int role;
+        private int publicity;
+    }
+}
---
+++ WebApp/WebApplication-PCMaker/WebApplication-PCMaker/Pages/accountSignIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication_PCMaker.Pages
+{
+    public class accountSignInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
