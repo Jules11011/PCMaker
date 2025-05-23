3b3acb317a29e74083055f67748613fca9f670b4
Jules <i476661@student.fontys.nl>
2025-05-23 15:07:05 +0200

    Added the connection string to the appsettings. Started a bit on making the connection with the database. Its not working, just pushing so that I can continue on my desktop.

---
+++ WebApp/WebApplication-PCMaker/DAL/AccountRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+	public class AccountRepository : BaseRepository
+	{
+		public AccountRepository(string connectionString) : base(connectionString)
+		{
+		}
+
+		//Use a model class for the account
+		public void AddAccount(string accountName)
+		{
+			using (var connection = GetConnection())
+			{
+				connection.Open();
+				var command = connection.CreateCommand();
+				command.CommandText = "INSERT INTO Accounts (Name) VALUES (@name)";
+				command.Parameters.AddWithValue("@name", accountName);
+				command.ExecuteNonQuery();
+			}
+		}
+	}
+}
---
+++ WebApp/WebApplication-PCMaker/DAL/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+	public abstract class BaseRepository
+	{
+		private readonly string _connectionString;
+		protected BaseRepository(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		protected SqlConnection GetConnection()
+		{
+			return new SqlConnection(_connectionString);
+		}
+	}
+}
